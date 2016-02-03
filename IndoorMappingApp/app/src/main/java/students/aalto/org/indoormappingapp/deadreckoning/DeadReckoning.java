c4a88ffbb890a5681b4f4b4d4b5961eec91b54a3
osaario <olli-matti.saario@aalto.fi>
2016-02-03 14:50:07 +0200

    initial skeletoning stuff

---
+++ IndoorMappingApp/app/src/main/java/students/aalto/org/indoormappingapp/deadreckoning/DeadReckoning.java
@@ -0,0 +1,13 @@
+package students.aalto.org.indoormappingapp.deadreckoning;
+
+import android.util.Pair;
+
+/**
+ * Created by olli-mattisaario on 3.2.16.
+ */
+public class DeadReckoning {
+    public static Pair<Integer, Integer> calculatePositionDelta(/*sensorinput*/) {
+
+        return null;
+    }
+}
---
+++ IndoorMappingApp/app/src/main/java/students/aalto/org/indoormappingapp/services/NetworkService.java
@@ -0,0 +1,21 @@
+package students.aalto.org.indoormappingapp.services;
+
+import java.util.ArrayList;
+import java.util.concurrent.FutureTask;
+
+import rx.Observable;
+
+/**
+ * Created by olli-mattisaario on 3.2.16.
+ */
+public class NetworkService {
+
+    public static Observable<Integer> sendStuffToBackend() {
+        return null;
+    }
+
+    public static Observable<ArrayList<Integer>> getBackendData() {
+        return null;
+    }
+
+}
