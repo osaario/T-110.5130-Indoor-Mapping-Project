271d3adcf938052bca872b12fb921d5e807594c1
Mehrad Mohammadi <mohammm4@aalto.fi>
2016-03-24 14:31:06 +0200

    interface improved

--- IndoorMappingApp/app/src/main/java/students/aalto/org/indoormappingapp/HomeActivity.java
+++ IndoorMappingApp/app/src/main/java/students/aalto/org/indoormappingapp/HomeActivity.java
@@ -31,10 +31,13 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home);
         addItemsToListView();
         addListenerOnListView();
+
     }
 
     public void addListenerOnListView(){
